3dadc7c9d45e14ad40b5c3666b12e2df89e228b0
muradcse1177 <muradcse1177@gmail.com>
2016-11-21 20:42:05 +0600

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,35 +1,16 @@
 #include<iostream>
-#include<cmath>
-#include<cstdlib>
 #include<cstdio>
-#include<algorithm>
-
 using namespace std;
-
-int print_array(int num[],int lm,int ul)
-    {
-        cout<<num[lm]<<" ";
-        if(lm!=ul)
-        {
-            print_array(num,lm+1,ul);
-        }
-        else
-        return 0;
-    }
 
 int main()
 {
-
-    int number[100];
-    int c;
-    cin>>c;
-    for(int i=1;i<=c;i++)
+    int n,i=1;
+    cin>>n;
+    while(i<=n)
     {
-        cin>>number[i];
+        cout<<i<<" ";
+        i++;
     }
-    print_array(number,1,c);
-
-
     return 0;
-
 }
+
